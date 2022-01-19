a5d427d2601115a696ed7d765b2441009128e236
IhsenBen <benali.ihsen@gmail.com>
2022-01-19 23:52:06 +0100

    -categories

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 /*  dotenv to ""hide sensitive data like api keys and credentials"*/
 dotenv.config();
@@ -21,7 +22,7 @@ mongoose
   app.use('/api/auth', authRoute);
   app.use('/api/users', userRoute);
   app.use('/api/posts', postRoute);
-
+  app.use('/api/categories', categoryRoute);
 
 
 app.listen('8000', () => {
