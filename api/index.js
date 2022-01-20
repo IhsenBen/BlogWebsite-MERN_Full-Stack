53f08fb244b67de59dd46ca0e011c166054c05b8
IhsenBen <benali.ihsen@gmail.com>
2022-01-20 01:31:06 +0100

    upload

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require("./routes/posts");
@@ -18,6 +19,26 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log('connected to mongo 🔌'))
   .catch((err) => console.log(err));
+
+  //storage for picture with multer pkg 
+  const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, 'img')},
+      filename:(req,file,cd)=>{
+        //save the original file name with current date and time
+   
+        const date = new Date();
+        //some  redundant way I found to get a readable date.() ( to imporove)
+        cd(null, date.getMonth()+'_'+ date.getDate()+'_'+ date.getFullYear()+'-'+date.getHours()+'_'+ date.getMinutes()+'-'+ file.originalname)
+      }},
+    );
+
+    const upload = multer({storage:storage});
+
+    app.post("/api/upload",upload.single('picture'),(req,res)=>{
+      res.status(200).json('uploaded');
+    });
+
 
   app.use('/api/auth', authRoute);
   app.use('/api/users', userRoute);
