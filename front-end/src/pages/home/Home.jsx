368ebc43331f1221b54ecf13baba1dd68f1fbc3c
IhsenBen <benali.ihsen@gmail.com>
2022-01-21 21:26:13 +0100

    beginning of fetch requests with axios

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -1,17 +1,28 @@
-import "./home.css"
-import Header from "../../components/header/Header"
-import Sidebar from "../../components/sidebar/Sidebar"
-import Posts from "../../components/posts/Posts"
+import './home.css';
+import { useEffect, useState } from 'react';
+import Header from '../../components/header/Header';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Posts from '../../components/posts/Posts';
+import axios from 'axios';
 
 export default function Home() {
-    return (
-        <>
-        <Header />
-        <div className="home">
-           
-            <Posts />
-            <Sidebar />
-        </div>
-        </>
-    )
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchedPosts = async () => {
+      const res = await axios.get('/posts');
+    //   console.log(res.data);
+ setPosts(res.data);
+    };
+    fetchedPosts();
+  }, []);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts}/>
+        <Sidebar />
+      </div>
+    </>
+  );
 }
